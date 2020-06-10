8301d30b92a46552c1872da40f1fa5ec7b05310f
Natalia Maximo <nmaximo@google.com>
2020-06-10 17:27:40 -0400

    Added a couple of tests

---
+++ tests/unit/test_config.py
@@ -0,0 +1,37 @@
+import pytest
+from acltest.utils.config import get_cli_configs
+
+
+class MockAbslFlags:
+    def __init__(self,
+                 max_qps=None,
+                 max_threads=None,
+                 pols=None,
+                 defs=None,
+                 policy_file=None,
+                 filename=None):
+        self.max_qps = max_qps
+        self.max_threads = max_threads
+        self.pols = pols
+        self.defs = defs
+        self.policy_file = policy_file
+        self.filename = filename
+
+
+@pytest.mark.unit
+def test_get_cli_configs_pols_dir():
+    expected = {
+        'load': {
+            'max_qps': 100,
+            'max_threads': 10,
+        },
+        'acl': {
+            'pols_location': 'pols/',
+            'defs_location': 'defs/'
+        }
+    }
+
+    flags = MockAbslFlags(max_qps=100, max_threads=10, pols='pols/', defs='defs/')
+    configs = get_cli_configs(flags)
+
+    assert configs == expected
