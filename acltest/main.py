84ef69653957e30e1867b0694845b6a1cafb0475
Natalia Maximo <nmaximo@google.com>
2020-06-12 11:25:19 -0400

    Moved flag setup to their own function

--- acltest/main.py
+++ acltest/main.py
@@ -7,19 +7,44 @@ from acltest.utils.config import get_configs_from_flags
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_multi_string('filename',
-                          ['acltest.yaml'],
-                          'Configuration input file(s)',
-                          short_name='f')
 
-flags.DEFINE_integer('max_qps',
-                     None,
-                     'Maximum number of queries per second to spawn',
-                     short_name='q')
-flags.DEFINE_integer('max_threads', None, 'Maximum number of threads to use', short_name='t')
-flags.DEFINE_string('pols', None, 'Location of the policies folder', short_name='p')
-flags.DEFINE_string('defs', None, 'Location of the definitions folder', short_name='d')
-flags.DEFINE_string('policy_file', None, 'Location to a single policy file to render')
+def setup_flags():
+    flags.DEFINE_multi_string(
+        'filename',
+        ['conf/acltest.yaml'],
+        'Configuration input file(s)',
+        short_name='f'
+    )
+
+    flags.DEFINE_integer(
+        'max_qps',
+        None,
+        'Maximum number of queries per second to spawn',
+        short_name='q'
+    )
+    flags.DEFINE_integer(
+        'max_threads',
+        None,
+        'Maximum number of threads to use',
+        short_name='t'
+    )
+    flags.DEFINE_string(
+        'pols',
+        None,
+        'Location of the policies folder',
+        short_name='p'
+    )
+    flags.DEFINE_string(
+        'defs',
+        None,
+        'Location of the definitions folder',
+        short_name='d'
+    )
+    flags.DEFINE_string(
+        'policy_file',
+        None,
+        'Location to a single policy file to render'
+    )
 
 
 def main(argv):
@@ -33,6 +58,7 @@ def get_module_script_path(module):
 
 
 def entry_point():
+    setup_flags()
     app.run(main)
 
 
